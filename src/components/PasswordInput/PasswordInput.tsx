1f37bb0986b6e1348e10efa796fc144ff53a7d79
seaerchin <jer005@e.ntu.edu.sg>
2021-05-19 02:50:11 +0800

    fix(components/passwordinput): fixed typing for passwordinput

--- src/components/PasswordInput/PasswordInput.tsx
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -1,22 +1,29 @@
 import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
-import React from 'react'
+import React, { forwardRef } from 'react'
 
-export const PasswordInput = (): JSX.Element => {
-  const [show, setShow] = React.useState(false)
-  const handleClick = () => setShow(!show)
+export const PasswordInput = forwardRef(
+  (
+    props: Record<string, unknown>,
+    ref: React.Ref<HTMLInputElement>,
+  ): JSX.Element => {
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
 
-  return (
-    <InputGroup size="md">
-      <Input
-        pr="4.5rem"
-        type={show ? 'text' : 'password'}
-        placeholder="Enter password"
-      />
-      <InputRightElement width="4.5rem">
-        <Button h="1.75rem" size="sm" onClick={handleClick}>
-          {show ? 'Hide' : 'Show'}
-        </Button>
-      </InputRightElement>
-    </InputGroup>
-  )
-}
+    return (
+      <InputGroup size="md">
+        <Input
+          {...props}
+          ref={ref}
+          pr="4.5rem"
+          type={show ? 'text' : 'password'}
+          placeholder="Enter password"
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleClick}>
+            {show ? 'Hide' : 'Show'}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    )
+  },
+)
