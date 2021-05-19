38b94b402e5714977f6b42c8be8205a4c50fe096
seaerchin <jer005@e.ntu.edu.sg>
2021-05-19 23:47:35 +0800

    refactor(contexts): cleaned up auth context

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,1 +1,30 @@
-export {}
+import { ApiService } from 'services/ApiService'
+import { JwtToken, LoginDetails, SignupDto, VerificationDto } from 'typings'
+
+export const signUpUser = (signupDetails: SignupDto): Promise<string> =>
+  ApiService.post<string>('/signup', signupDetails).then(({ data }) => {
+    return data
+  })
+
+export const verifyOtp = ({
+  otp,
+  email,
+}: VerificationDto): Promise<boolean> => {
+  return ApiService.post<boolean>('/verify', {
+    email,
+    verificationKey: otp,
+  }).then(({ data }) => data)
+}
+
+export const loginUser = (
+  loginDetails: LoginDetails,
+): Promise<{ token: JwtToken }> => {
+  return ApiService.post<{ token: JwtToken }>('/login', loginDetails).then(
+    ({ data }) => {
+      return data
+    },
+  )
+}
+
+// TODO: ping backend
+export const logoutUser = (): Promise<void> => ApiService.post('/logout')
