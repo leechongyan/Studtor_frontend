18aa7608f899d21230803bb9aefcddfb25595940
seaerchin <jer005@e.ntu.edu.sg>
2021-05-20 04:01:11 +0800

    docs(authcontext): updated docs

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -32,7 +32,7 @@ const AuthContext = createContext<AuthContextProps | undefined>(undefined)
 export const useAuth = (): AuthContextProps => {
   const context = useContext(AuthContext)
   if (!context) {
-    throw new Error(`useAuth must be used within a ProvideAuth component`)
+    throw new Error(`useAuth must be used within a AuthProvider`)
   }
   return context
 }
